6150a6b7cdf8b5e65b8930ce54e525583fb0226c
Michael Adeyemo <michael.adesola1410@gmail.com>
2024-02-02 22:06:41 +0100

    rework signup

--- lib/ui/onboarding/pages/sign_up_step_one.dart
+++ lib/ui/onboarding/pages/sign_up_step_one.dart
@@ -18,51 +18,47 @@ class SignUpStepOne extends StatelessWidget {
     return ScaffoldPagePaddingWidget(
       horizontal: 24,
       child: Scaffold(
-        appBar: AppBarWidget(
+        appBar: const AppBarWidget(
           title: 'Sign up',
         ),
-        body: Column(
-          children: [
-            Expanded(
-              flex: 20,
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 32),
-                      Text(
-                        "Create a Xcrow profile with a few details. Your password must have at least 8 characters including letters and a number.",
-                        style: context.bodyMedium?.copyWith(
-                            fontSize: 13,
-                            fontWeight: FontWeight.w300,
-                            height: 16.4 / 14),
-                      ),
-                      SizedBox(height: 32),
-                      Consumer(
-                        builder: (BuildContext context, WidgetRef ref,
-                            Widget? child) {
-                          final index = ref.watch(_pageIndexProvider);
-                          return IndexedStack(
-                            index: index,
-                            children: [
-                              _StepOneWidget(),
-                              child!,
-                            ],
-                          );
-                        },
-                        child: _StepTwoWidget(),
-                      ),
-                    ],
-                  ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 32),
+                    Text(
+                      "Create a Xcrow profile with a few details. Your password must have at least 8 characters including letters and a number.",
+                      style: context.bodyMedium?.copyWith(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w300,
+                          height: 16.4 / 14),
+                    ),
+                    const SizedBox(height: 32),
+                    Consumer(
+                      builder:
+                          (BuildContext context, WidgetRef ref, Widget? child) {
+                        final index = ref.watch(_pageIndexProvider);
+                        return IndexedStack(
+                          index: index,
+                          children: [
+                            const _StepOneWidget(),
+                            child!,
+                          ],
+                        );
+                      },
+                      child: const _StepTwoWidget(),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            Spacer(),
-            SocialButtons(),
-            const SizedBox(height: 16),
-          ],
+              const SocialButtons(),
+              const SizedBox(height: 16),
+            ],
+          ),
         ),
       ),
     );
@@ -88,26 +84,26 @@ class _StepOneWidget extends ConsumerWidget {
           ),
         ),
         const SizedBox(height: 20),
-        TextField(
+        const TextField(
           decoration: InputDecoration(hintText: '+234 xxxxxxxxxxxx'),
         ),
         const SizedBox(height: 20),
-        TextField(
+        const TextField(
           decoration: InputDecoration(hintText: 'English'),
         ),
         const SizedBox(height: 20),
-        TextField(
+        const TextField(
           decoration: InputDecoration(hintText: 'Email Address'),
         ),
-        SizedBox(height: 52),
+        const SizedBox(height: 52),
         AppButton.elevatedButton(
           label: 'Create an Account',
           onPressed: () {
             ref.read(_pageIndexProvider.notifier).state = 1;
           },
         ),
-        SizedBox(height: 24),
-        _AlreadyHasAccount()
+        const SizedBox(height: 24),
+        const _AlreadyHasAccount()
       ],
     );
   }
@@ -132,11 +128,11 @@ class _StepTwoWidget extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 20),
-        TextField(
+        const TextField(
           decoration: InputDecoration(hintText: 'First name'),
         ),
         const SizedBox(height: 20),
-        TextField(
+        const TextField(
           decoration: InputDecoration(hintText: 'Surname name'),
         ),
         const SizedBox(height: 20),
@@ -148,18 +144,18 @@ class _StepTwoWidget extends StatelessWidget {
           },
         ),
         const SizedBox(height: 20),
-        TextField(
+        const TextField(
           decoration: InputDecoration(hintText: 'User Name'),
         ),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         AppButton.elevatedButton(
           label: 'Next',
           onPressed: () {
-            context.push(ConfirmEmailPage());
+            context.push(const ConfirmEmailPage());
           },
         ),
-        SizedBox(height: 24),
-        _AlreadyHasAccount()
+        const SizedBox(height: 24),
+        const _AlreadyHasAccount()
       ],
     );
   }
@@ -170,7 +166,7 @@ class _AlreadyHasAccount extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextActionRow(
+    return const TextActionRow(
       title: 'Already have an account? ',
       action: 'Login',
       mainAxisAlignment: MainAxisAlignment.center,
