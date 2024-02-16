5fd23dd70ea599beba53c7e279b36d6e2dec1264
Michael Adeyemo <michael.adesola1410@gmail.com>
2024-02-16 17:52:09 +0100

    account page

--- lib/ui/account/pages/account_page.dart
+++ lib/ui/account/pages/account_page.dart
@@ -23,48 +23,54 @@ class AccountPage extends StatelessWidget {
             width: 307,
             child: Column(
               children: [
-                const SizedBox(height: 8),
-                const AppBarCircle(
-                  size: 60,
-                ),
-                const SizedBox(height: 12),
-                Text('Tier 1 Upgrade Account'.toUpperCase()),
-                const SizedBox(height: 12),
-                const UsernameWidget(),
-                const SizedBox(height: 14),
-                AppButton.elevatedButton(
-                  label: 'Edit button',
-                  onPressed: () {},
-                ),
-                const SizedBox(height: 24),
-                const AccountAction(
-                  svgIcon: SvgPath.account,
-                  title: 'Account information',
-                ),
-                const Spacer(),
-                const AccountAction(
-                  svgIcon: SvgPath.beneficiary,
-                  title: 'Beneficiary',
-                ),
-                const Spacer(),
-                const AccountAction(
-                  svgIcon: SvgPath.security,
-                  title: 'Security',
-                ),
-                const Spacer(),
-                const AccountAction(
-                  svgIcon: SvgPath.identity,
-                  title: 'Identification',
-                ),
-                const Spacer(),
-                const AccountAction(
-                  svgIcon: SvgPath.call,
-                  title: 'Help and support',
-                ),
-                const Spacer(),
-                const AccountAction(
-                  svgIcon: SvgPath.social,
-                  title: 'Social Media',
+                SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 8),
+                      const AppBarCircle(
+                        size: 60,
+                      ),
+                      const SizedBox(height: 12),
+                      Text('Tier 1 Upgrade Account'.toUpperCase()),
+                      const SizedBox(height: 12),
+                      const UsernameWidget(),
+                      const SizedBox(height: 14),
+                      AppButton.elevatedButton(
+                        label: 'Edit button',
+                        onPressed: () {},
+                      ),
+                      const SizedBox(height: 24),
+                      const AccountAction(
+                        svgIcon: SvgPath.account,
+                        title: 'Account information',
+                      ),
+                      const SizedBox(height: 16),
+                      const AccountAction(
+                        svgIcon: SvgPath.beneficiary,
+                        title: 'Beneficiary',
+                      ),
+                      const SizedBox(height: 16),
+                      const AccountAction(
+                        svgIcon: SvgPath.security,
+                        title: 'Security',
+                      ),
+                      const SizedBox(height: 16),
+                      const AccountAction(
+                        svgIcon: SvgPath.identity,
+                        title: 'Identification',
+                      ),
+                      const SizedBox(height: 16),
+                      const AccountAction(
+                        svgIcon: SvgPath.call,
+                        title: 'Help and support',
+                      ),
+                      const SizedBox(height: 16),
+                      const AccountAction(
+                        svgIcon: SvgPath.social,
+                        title: 'Social Media',
+                      ),
+                    ],
+                  ),
                 ),
                 const Spacer(),
                 const Row(
